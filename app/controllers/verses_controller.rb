44811ee80e586e3a1add31a982983ff834a84f46
Dmitry Grigoriev <dmitry@dxfoto.ru>
2011-12-18 17:31:40 +0400

    verses are now createable, viewable and showed on home page

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,7 +1,27 @@
 class VersesController < ApplicationController
 
+  def index
+  
+  end
+  
+  def show
+    @verse = Verse.find(params[:id])
+  end
+
   def new
     @verse = Verse.new
+  end
+  
+  def create
+    @verse = Verse.new params[:verse]
+    @verse.user = current_user
+    if @verse.save
+      flash[:success] = t(:'verses.create.success')
+      redirect_to verse_path(@verse)
+    else
+      flash[:error] = t(:'verses.create.failure')
+      render :action => 'new'
+    end
   end
 
 end
