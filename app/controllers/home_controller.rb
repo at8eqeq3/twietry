d7aab6d30df1b2a3ea10a1963271cfa4dfe58b33
Dmitry Grigoriev <dmitry@dxfoto.ru>
2012-01-15 12:12:55 +0400

    there are short statistic message and a list of top-rated users on home#index now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @closed_verses = Verse.where(:is_finished => true)
-    @open_verses = Verse.where(:is_finished => false)
     @activities = Activity.all.order_by([[:created_at, :desc]]).limit(10)
+    @verses_count = Verse.count
+    @users = User.order_by([[:rating, :desc]]).limit(15)
+    @users_count = User.count
+    @lines_count = 0
+    # soooo saaaad...
+    Verse.all.each do |verse|
+      @lines_count += verse.lines.count
+    end
   end
 
   def about
